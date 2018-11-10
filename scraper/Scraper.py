8f68f809748acb63583d11ee0e96ae5237417d59
edpity <ed.pitt@internode.on.net>
2018-11-10 17:31:35 +1100

    Fully working for polling locations
    Now prints all booths for each electorate listing the IDs and names for both.
    To do: Print to csv format.

--- scraper/Scraper.py
+++ scraper/Scraper.py
@@ -1,20 +1,21 @@
 import os ###Required to change directory
-os.chdir('C:/location/etc') ###Set directory
+os.chdir('GitHubLocation/VEC2018/scraper/set-up') ###Set directory
 import lxml
 from lxml import etree
 import xml.etree.ElementTree as ET ###Will parse xml
 import requests ###Requests will be used for the VEC site, not utilised at this stage
-tree = ET.ElementTree(file='State2018MediaFilePollingLocations.xml') ###Loads file
+tree = ET.parse('State2018MediaFilePollingLocations.xml') ###Loads file
 root = tree.getroot()
 
-TagSlug = '{http://www.aec.gov.au/xml/schema/mediafeed}' ###Required to append root name
 
-PollingDistricts = root.findall(TagSlug+'PollingDistrict')
-for PollingDistrict in PollingDistricts:
-    DistrictID = PollingDistrict.find(TagSlug+'PollingDistrictIdentifier')
-    Name = DistrictID.find(TagSlug+'Name')
-    print(DistrictID.attrib['Id'], Name.text)
-    
-    ###Should print electorates and their IDs
+TagSlug = '{http://www.aec.gov.au/xml/schema/mediafeed}' #This is pre-appended all nodes so saves space
 
-
+PollingDistricts = root.findall(TagSlug+'PollingDistrict') #Goes from level 0 (root) to level 1 (PollingDistrict)
+for PollingDistrict in PollingDistricts: #Required otherwise only the first district would display
+    DistrictID = PollingDistrict.find(TagSlug+'PollingDistrictIdentifier') #Finds the district ID
+    Name = DistrictID.find(TagSlug+'Name') #Finds the name of each electorate (as a child of DistrictID)
+    PollingPlaces = PollingDistrict.find(TagSlug+'PollingPlaces') 
+    PollingPlace = PollingPlaces.find(TagSlug+'PollingPlace') #These two lines are ONLY for navigating the XML file
+    for PollingPlace in PollingPlaces: #Required otherwise it would only print the first booth in each electorate
+        PPID = PollingPlace.find(TagSlug+'PollingPlaceIdentifier') #Finds both the booth ID and name
+        print(PPID.attrib['Id'], PPID.attrib['Name'], DistrictID.attrib['Id'], Name.text) #Prints the text
